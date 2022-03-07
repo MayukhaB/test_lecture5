250ad42e5c164385f06aa8b36c7b98dcffb4ef03
MayukhaB <88716227+MayukhaB@users.noreply.github.com>
2022-03-07 15:18:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 from datetime import date
 from xml.dom.minidom import CharacterData
+from altair_data_server import data_server
 alt.data_transformers.enable('data_server')
-#alt.data_transformers.disable_max_rows()
+alt.data_transformers.disable_max_rows()
 
 
 
